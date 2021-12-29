a89a7351f8d767d68a7c9f42340bc99ecd1cf7ce
v1j4y <vijay.gopal.c@gmail.com>
2021-12-29 17:59:48 +0100

    Compiles.

--- build_dgemm.sh
+++ build_dgemm.sh
@@ -3,7 +3,7 @@
 #CC=${CC:-icpc}
 CC=icpc
 CCFLAGS=(
- -O3 
+ -O3
  -g
  -march=core-avx2
  -finline
@@ -19,49 +19,37 @@ CCFLAGS=(
 # -mcmodel=medium
 # -DMKL_DIRECT_CALL_SEQ_JIT
 #-I/home/vijayc/gemm_asm/MIPP/src -I.
-MKL_PATH=/share/apps/intel/oneapi/mkl/2021.1.1/
-OMP_PATH=/share/apps/intel/oneapi/mkl/2021.1.1/
-MKL_PATH=/opt/intel/compilers_and_libraries_2019.3.199/linux/mkl
-MKL_LIB="-Wl,--start-group ${MKL_PATH}/lib/intel64/libmkl_intel_lp64.a ${MKL_PATH}/lib/intel64/libmkl_intel_thread.a ${MKL_PATH}/lib/intel64/libmkl_core.a -Wl,--end-group"
+MKL_PATH=/opt/intel/oneapi/mkl/2021.4.0/
+OMP_PATH=/opt/intel/oneapi/mkl/2021.4.0/
+#MKL_PATH=/opt/intel/compilers_and_libraries_2019.3.199/linux/mkl
+#MKL_LIB="-Wl,--start-group ${MKL_PATH}/lib/intel64/libmkl_intel_lp64.a ${MKL_PATH}/lib/intel64/libmkl_intel_thread.a ${MKL_PATH}/lib/intel64/libmkl_core.a -Wl,--end-group"
 MKL_LIB="-mkl -static-intel -no-intel-extensions -qopenmp-link=static -static-libgcc -static-libstdc++ -Wl,--start-group ${MKL_PATH}/lib/intel64/libmkl_intel_lp64.a ${MKL_PATH}/lib/intel64/libmkl_intel_thread.a ${MKL_PATH}/lib/intel64/libmkl_core.a -Wl,--end-group"
 #MKL_LIB="-mkl=sequential"
 echo $MKL_PATH
 echo $MKL_LIB
 
-OBJ_CMD="-c "${CCFLAGS[@]}" utils.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
+OBJ_CMD="-c "${CCFLAGS[@]}" src/utils.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
 echo $CC $OBJ_CMD
 $CC $OBJ_CMD
 
-#OBJ_CMD="-c "${CCFLAGS[@]}" kernel.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread  "${LFLAGS[@]}" "${@}
+OBJ_CMD="-c "${CCFLAGS[@]}" src/kernel_sse2_8regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
+echo $CC $OBJ_CMD
+$CC $OBJ_CMD
+
+OBJ_CMD="-c "${CCFLAGS[@]}" src/kernel_avx2_8regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
+echo $CC $OBJ_CMD
+$CC $OBJ_CMD
+
+OBJ_CMD="-c "${CCFLAGS[@]}" src/kernel_avx2_16regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
+echo $CC $OBJ_CMD
+$CC $OBJ_CMD
+
+#OBJ_CMD="-c "${CCFLAGS[@]}" src/kernel_avx2_12x4_16regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
 #echo $CC $OBJ_CMD
 #$CC $OBJ_CMD
 
-OBJ_CMD="-c "${CCFLAGS[@]}" kernel_sse2_8regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
-echo $CC $OBJ_CMD
-$CC $OBJ_CMD
-
-OBJ_CMD="-c "${CCFLAGS[@]}" kernel_avx2_8regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
-echo $CC $OBJ_CMD
-$CC $OBJ_CMD
-
-#OBJ_CMD="-c "${CCFLAGS[@]}" kernel_sse2_16regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread  "${LFLAGS[@]}" "${@}
-#echo $CC $OBJ_CMD
-#$CC $OBJ_CMD
-
-OBJ_CMD="-c "${CCFLAGS[@]}" kernel_avx2_16regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
-echo $CC $OBJ_CMD
-$CC $OBJ_CMD
-
-OBJ_CMD="-c "${CCFLAGS[@]}" kernel_avx2_12x4_16regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
-echo $CC $OBJ_CMD
-$CC $OBJ_CMD
-
-#OBJ_CMD="-c "${CCFLAGS[@]}" kernel_avx2_32regs.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread  "${LFLAGS[@]}" "${@}
-#echo $CC $OBJ_CMD
-#$CC $OBJ_CMD
-
-OBJ_CMD="-c "${CCFLAGS[@]}" main.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
-BUILD_CMD="-o xtest "${CCFLAGS[@]}" main.c kernel_avx2_8regs.o kernel_sse2_8regs.o kernel_avx2_12x4_16regs.o kernel_avx2_16regs.o utils.o -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
+OBJ_CMD="-c "${CCFLAGS[@]}" src/main.c -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
+BUILD_CMD="-o xtest "${CCFLAGS[@]}" src/main.c kernel_avx2_8regs.o kernel_sse2_8regs.o kernel_avx2_16regs.o utils.o -I"${MKL_PATH}" "${MKL_LIB}" -L"${OMP_PATH}" -qopenmp -lpthread "${LFLAGS[@]}" "${@}
 echo $CC $OBJ_CMD
 echo $CC $BUILD_CMD
 $CC $OBJ_CMD
